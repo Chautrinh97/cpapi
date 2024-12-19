1be61a30b2dc9768160ea03093e88e64ff0e9594
Chautrinh97 <trinhdc197@gmail.com>
2024-12-20 01:36:31 +0700

    a

--- src/modules/auth/strategies/jwt.strategy.ts
+++ src/modules/auth/strategies/jwt.strategy.ts
@@ -26,6 +26,8 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     if (req.cookies && req.cookies.accessToken) {
       return req.cookies.accessToken;
     }
+
+    console.log('NO COOKIE FOUND');
     return null;
   }
 }
